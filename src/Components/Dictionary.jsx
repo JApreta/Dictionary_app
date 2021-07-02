a19f0f9628af99b5247d7829019f8f6b2fb179a0
JAntonio <judithantonio@email.arizona.edu>
2021-07-02 07:46:46 -0700

    API Integration

--- src/Components/Dictionary.jsx
+++ src/Components/Dictionary.jsx
@@ -1,15 +1,24 @@
 import React, { useState } from "react"
+import axios from "axios";
 import "../Styles/Dictionary.css"
 
 export function Dictionary() {
     const [keyword, setKeyword] = useState('love');
+    const language = "en_US";
+
     function FormSubmissionHandler(e) {
         e.preventDefault();
+        const apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/${language}/${keyword}`
+        axios.get(apiUrl).then(handleResponse);
         alert(`you are searching for ${keyword} definition`)
 
     }
     function KeywordChangeHandler(e) {
         setKeyword(e.target.value);
+    }
+    function handleResponse(response) {
+        console.log(response.data[0]);
+
     }
     return (
         <div className="Dictionary">
